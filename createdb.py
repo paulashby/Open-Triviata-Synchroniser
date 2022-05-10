ec9c6234e1a3f84c87dfd96f39199bc434012ac5
Paul Ashby <paul@primitive.co>
2022-05-10 15:25:51 +0100

    Make tables after creating database For database credentials, accept name of config file from command line

--- createdb.py
+++ createdb.py
@@ -1,24 +1,61 @@
 import sys
+import argparse
 import configparser
 from mysql.connector import connect, Error
 
+# Parse command line args
+parser = argparse.ArgumentParser(description='Get MySQL credentials')
+parser.add_argument('-c', '--config', help='name of config file containing MySQL credentials', required=True)
+args = parser.parse_args()
+
+# Config file to read for database credentials
+configfilename = getattr(args, 'config')
+
+# Name of section to parse from config file
+configname = configfilename.replace('.ini', '')
 config = configparser.ConfigParser()
+config.read(configfilename)
 
-config.read('opentriviata.ini')
-
-if not 'opentriviata' in config:
+if not configname in config:
     print("Unable to access database credentials.")
     sys.exit()
 
 try:
     with connect(
+
         # Use database credentials from .ini
-        host=config['opentriviata']['Host'],
-        user=config['opentriviata']['User'],
-        password=config['opentriviata']['Pass'],
+        host=config[configname]['Host'],
+        user=config[configname]['User'],
+        password=config[configname]['Pass']
+
     ) as connection:
-        create_db_query = "CREATE DATABASE opentriviata"
+        
+        db_query = "CREATE DATABASE IF NOT EXISTS opentriviata"
         with connection.cursor() as cursor:
-            cursor.execute(create_db_query)
+            cursor.execute(db_query)
+
+except Error as e:
+    print(e)
+
+# Add tables
+try:
+    with connect(
+        # Use database credentials from .ini
+        host=config[configname]['Host'],
+        user=config[configname]['User'],
+        password=config[configname]['Pass'],
+        database="opentriviata",
+    ) as connection:
+        
+        db_queries = [
+            "CREATE TABLE categories (id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL, category VARCHAR(255) NOT NULL UNIQUE)",
+            "ALTER TABLE categories AUTO_INCREMENT=9",
+            "CREATE TABLE questions (id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL, category_id INTEGER NOT NULL, type VARCHAR(16) NOT NULL, difficulty VARCHAR(16) NOT NULL, question_text VARCHAR(1024) NOT NULL UNIQUE, FOREIGN KEY(category_id) REFERENCES categories(id))",
+            "CREATE TABLE answers (question_id INTEGER NOT NULL, answer VARCHAR(1024) NOT NULL, correct BOOLEAN NOT NULL, FOREIGN KEY(question_id) REFERENCES questions(id))"
+        ]
+        with connection.cursor() as cursor:
+            for db_query in db_queries:
+                cursor.execute(db_query)
+
 except Error as e:
     print(e)
