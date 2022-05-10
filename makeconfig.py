4799d45aa0a8ea3d66596f355416019fbc05cfed
Paul Ashby <paul@primitive.co>
2022-05-10 15:02:37 +0100

    Accept command line arguments for name of output file and database credentials

--- makeconfig.py
+++ makeconfig.py
@@ -1,10 +1,21 @@
+import argparse
 import configparser
+
+# Parse command line args
+parser = argparse.ArgumentParser(description='Get settings for ini file')
+parser.add_argument('-f', '--filename', help='filename for the generated ini file', default='dbconfig')
+parser.add_argument('-w', '--hostname', help='hostname', default='localhost')
+parser.add_argument('-u', '--username', help='username', default='root')
+parser.add_argument('-p', '--password', help='password', required=True)
+args = parser.parse_args()
+configname = getattr(args, 'filename')
+
 config = configparser.ConfigParser()
 
 # Using <placeholders> - replace these with live values before creating ini file
-config['opentriviata'] = {}
-config['opentriviata']['Host'] = '<host>'
-config['opentriviata']['User'] = '<username>'
-config['opentriviata']['Pass'] = '<password>'
-with open('opentriviata.ini', 'w') as configfile:
+config[configname] = {}
+config[configname]['Host'] = getattr(args, 'hostname')
+config[configname]['User'] = getattr(args, 'username')
+config[configname]['Pass'] = getattr(args, 'password')
+with open(configname + '.ini', 'w') as configfile:
   config.write(configfile)
