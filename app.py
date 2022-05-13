d61aca4c9172ece1ffbfad0632038e0e56444db4
Paul Ashby <paul@primitive.co>
2022-05-13 11:19:43 +0100

    Add main, remove redundant comment

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from helpers import get_session_token, reset_session_token
 
-"""
-    • Should we rename this app.py?
-    • Do we need a main()?
-        https://realpython.com/python-main-function/
-"""
-token = get_session_token()
-print(f"token = {token}")
+def main():
+    token = get_session_token()
+    print(f"token = {token}")
+
+if __name__ == "__main__":
+    main()
